64172e9696f8db244a803c121fa2a85df595eb3c
AJ-Buys <61046581+AJ-Buys@users.noreply.github.com>
2020-10-06 15:32:37 +0200

    Added SteamApi

--- Main.js
+++ Main.js
@@ -1,7 +1,15 @@
 
+window.onload = loading();
+    
+function loading(){
+    getAnime();
+    getSteam();
+
+}
 
 /* Anime API */
-fetch('https://api.jikan.moe/v3/search/anime?q=yugioh').then(res => res.json()).then((json)=>{
+function getAnime(){
+    fetch('https://api.jikan.moe/v3/search/anime?q=yugioh').then(res => res.json()).then((json)=>{
         var anime_data = json.results
         anime_data.length = 3
     
@@ -10,12 +18,12 @@ fetch('https://api.jikan.moe/v3/search/anime?q=yugioh').then(res => res.json()).
         {
             return `
                 <div class="flex-row">
-                    <a href="${anime.url}" class="anime_cont">
+                    <a href="${anime.url}" class="anime_con" target="_blank">
                         <div class"anime_left">
                             <img src="${anime.image_url}">
                         </div>
                         <div class"anime_right">
-                            <div class="anime_name">${anime.title}
+                            <h3>${anime.title}</h3>
                         </div>
                     </div>
                     </a>
@@ -24,34 +32,39 @@ fetch('https://api.jikan.moe/v3/search/anime?q=yugioh').then(res => res.json()).
             `
         }
         
-        var ani_con = document.getElementById("anime").innerHTML = `
-        <h1> ${anime_data.length} " results were found"</h1>
+        document.getElementById("anime").innerHTML = `
+        <h2> ${anime_data.length} " results were found"</h2>
         ${anime_data.map(anime_layout)}
         `
  
     })
+}
+
+/* Steam API */
+function getSteam(){
+    fetch('https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=359550&count=3&maxlength=300&format=json').then(res => res.json()).then((json)=>{
+    var steam_data = json.appnews.newsitems
     
-/* Steam API 
-fetch('').then(res => res.json()).then((json)=>{
-    var steam_data = json.results
 
     function steam_layout(item)
     {
         return `
-        
-        
+                 <div class="flex-row">
+                    <a href="${item.url}" class="steam_con">
+                            <div class="steam_cont">${item.title}</div>
+                    </a>
+                <div>
         `
     }
 
-
-    var steam_con = decument.getElementById("").innerHTML = `
-    
-    
+    document.getElementById("steam").innerHTML = `
+    <h2>This is the latest news concerning one of my favorite games</h2>
+    ${steam_data.map(steam_layout)}
     `
+})
 
-
-    */
- 
+}
+    
 
 /* Fun facts 
     fetch('').then(res => res.json()).then((json)=>{
--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 
-function removeLoader(){
-    $( "#loader" ).fadeOut(500, function() {
-      // fadeOut complete. Remove the loading div
-      $( "#loader" ).remove(); //makes page more lightweight 
-  });  
 
-$('body').append('div class="loader">Loading...</div></div>');
-$(window).on('load', function(){
-  setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
-});
 
-}
